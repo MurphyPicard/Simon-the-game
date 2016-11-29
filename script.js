404fdc15f68207c7bb268cfec1256a65b5c9563f
murphypicard <ayaghsizian@gmail.com>
2016-11-29 17:03:44 -0500

    fixed link and some add to array type functions

--- script.js
+++ script.js
@@ -2,12 +2,47 @@
 //nothing yet
 
 //define some variables
-let computerChoice = [];
-let userChoice = [];
+let computerChoices = [];//will be an array of colors
+let userChoices = [];//will be an array of colors
+let mostRecentChoice = [];
 
-let startGame = document.getElementById('startButton');
+let startGameButton = document.getElementById('startButton');
 
 let greenButton = document.getElementById('greenButton');
 let redButton = document.getElementById('redButton');
 let yellowButton = document.getElementById('yellowButton');
 let blueButton = document.getElementById('blueButton');
+
+
+
+let pushToComp = function (){
+  var randomNumber = Math.floor((Math.random() * 4) + 1);
+  computerChoices.push(randomNumber);
+};//pushToComp function
+
+startGameButton.addEventListener('click', pushToComp);
+
+
+greenButton.addEventListener('click', function(){
+mostRecentChoice = 1;
+userChoices.push(mostRecentChoice); } );
+
+redButton.addEventListener('click', function(){
+mostRecentChoice = 2;
+userChoices.push(mostRecentChoice); } );
+
+yellowButton.addEventListener('click', function(){
+mostRecentChoice = 3;
+userChoices.push(mostRecentChoice); } );
+
+blueButton.addEventListener('click', function(){
+mostRecentChoice = 4;
+userChoices.push(mostRecentChoice); } );
+
+let pushToUser = function (){
+};//pushToUser function
+
+
+
+
+//startGameButton.addEventListener('click', aIChooseColor);
