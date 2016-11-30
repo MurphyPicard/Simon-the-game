61158ad9edc1b7a4bdd142072ddf9fdc38c99099
murphypicard <ayaghsizian@gmail.com>
2016-11-30 14:49:50 -0500

    trying to work on user button clicks

--- script.js
+++ script.js
@@ -15,24 +15,40 @@ let roundNumber = 1;
 
 
 
-
-
 startGameButton.addEventListener('click', computersFirstChoice);
 //add initial number to computerChoices array
 
 function computersFirstChoice(){
+  computerChoices = [];
+  userChoices = [];
   roundNumber = 1;
   randomNumber = Math.floor((Math.random() * 4) + 1);
   computerChoices.push(randomNumber);
-  highlight(randomNumber);
+
+  var interval = setInterval(function(){
+    highlight(randomNumber);
+  }, 500);
+
+  var interval2 = setInterval(function(){
+    clearInterval(interval);
+    unhighlight(randomNumber);
+  }, 1000);
+
+  var interval3 = setInterval(function(){
+    clearInterval(interval2);
+  }, 1001);
   //beep
-}
+}//computersFirstChoice
+
 
 function playersFirstChoice(){
   greenButton.addEventListener('click', greenFunction);
   redButton.addEventListener('click', redFunction);
   yellowButton.addEventListener('click', yellowFunction);
   blueButton.addEventListener('click', blueFunction);
+  decide();
+
+  console.log("com array: "+computerChoices+" , u array: "+ userChoices+ ".");
 
 }
 
@@ -73,9 +89,7 @@ function decide (){
       }//else
 }//decidefunction
 
-var intervalId = setInterval(function(){
-  highlight();
-}, 400);
+
 
 
 
@@ -83,21 +97,36 @@ var intervalId = setInterval(function(){
 function highlight (num){
   switch(num){
     case 1:
-
-      greenButton.style.backgroundColor = "pink";
+      greenButton.style.backgroundColor = "#0a0";
       break;
     case 2:
-      redButton.style.backgroundColor = "pink";
+      redButton.style.backgroundColor = "#a00";
       break;
     case 3:
-      yellowButton.style.backgroundColor = "pink";
+      yellowButton.style.backgroundColor = "#aa0";
       break;
     case 4:
-      blueButton.style.backgroundColor = "pink";
+      blueButton.style.backgroundColor = "#00a";
       break;
   }//switch
 }//highlight function
 
+function unhighlight (num){
+  switch(num){
+    case 1:
+      greenButton.style.backgroundColor = "green";
+      break;
+    case 2:
+      redButton.style.backgroundColor = "red";
+      break;
+    case 3:
+      yellowButton.style.backgroundColor = "yellow";
+      break;
+    case 4:
+      blueButton.style.backgroundColor = "blue";
+      break;
+  }//switch
+}
 
 
 
