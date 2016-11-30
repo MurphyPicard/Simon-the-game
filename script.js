5c9ffe6eb18400b10419572764b93a4c61849e67
murphypicard <ayaghsizian@gmail.com>
2016-11-29 20:55:22 -0500

    decide function, need to do more turns now

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let userChoices = [];//will be an array of colors
 let mostRecentChoice = [];
 
 let startGameButton = document.getElementById('startButton');
+let continueGame = false;
 
 let greenButton = document.getElementById('greenButton');
 let redButton = document.getElementById('redButton');
@@ -18,29 +19,67 @@ let blueButton = document.getElementById('blueButton');
 let pushToComp = function (){
   var randomNumber = Math.floor((Math.random() * 4) + 1);
   computerChoices.push(randomNumber);
+  continueGame = true;
 };//pushToComp function
-
 startGameButton.addEventListener('click', pushToComp);
+//add initial number to computerChoices array
+console.log(continueGame);
 
 
-greenButton.addEventListener('click', function(){
-mostRecentChoice = 1;
-userChoices.push(mostRecentChoice); } );
+let greenFunction = function (){
+  mostRecentChoice = 1;
+  userChoices.push(mostRecentChoice);
+};
+let redFunction = function (){
+  mostRecentChoice = 2;
+  userChoices.push(mostRecentChoice);
+};
+let yellowFunction = function (){
+  mostRecentChoice = 3;
+  userChoices.push(mostRecentChoice);
+};
+let blueFunction = function (){
+  mostRecentChoice = 4;
+  userChoices.push(mostRecentChoice);
+};
+greenButton.addEventListener('click', greenFunction);
+redButton.addEventListener('click', redFunction);
+yellowButton.addEventListener('click', yellowFunction);
+blueButton.addEventListener('click', blueFunction);
 
-redButton.addEventListener('click', function(){
-mostRecentChoice = 2;
-userChoices.push(mostRecentChoice); } );
+  // greenButton.addEventListener('click', function(){
+  // mostRecentChoice = 1;
+  // userChoices.push(mostRecentChoice); } );
+  // redButton.addEventListener('click', function(){
+  // mostRecentChoice = 2;
+  // userChoices.push(mostRecentChoice); } );
+  // yellowButton.addEventListener('click', function(){
+  // mostRecentChoice = 3;
+  // userChoices.push(mostRecentChoice); } );
+  // blueButton.addEventListener('click', function(){
+  // mostRecentChoice = 4;
+  // userChoices.push(mostRecentChoice); } );
+// }//while
 
-yellowButton.addEventListener('click', function(){
-mostRecentChoice = 3;
-userChoices.push(mostRecentChoice); } );
+function decide (){
 
-blueButton.addEventListener('click', function(){
-mostRecentChoice = 4;
-userChoices.push(mostRecentChoice); } );
 
-let pushToUser = function (){
-};//pushToUser function
+
+      if (userChoices[userChoices.length - 1] !== computerChoices[computerChoices.length - 1]){
+        console.log(computerChoices, userChoices, "sorry");
+
+      }//if
+      else {
+        console.log(computerChoices, userChoices, "yay");
+
+      }//else
+
+
+
+}//decidefunction
+
+// let pushToUser = function (){
+// };//pushToUser function
 
 
 
